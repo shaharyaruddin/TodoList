17ac33aadcad94893f3e8f28aa81642ad462a5dd
MOD Administrator <admin@M365x22087644.onmicrosoft.com>
2024-02-29 23:17:16 +0500

    Apply Maping

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdCloseCircle } from "react-icons/io";
 import { IoIosAddCircle } from "react-icons/io";
 function Todo() {
+
+const [list,setList]=useState("")
+const [items,setItems]=useState([])
+
+const handleItems=(e)=>{
+    
+
+    const AddedItems=e.target.value
+    
+    setList(AddedItems)
+
+}
+
+const handleClick =()=>{
+    setItems((todoItems)=>{
+return [...todoItems,list]
+
+    })
+    setList("")
+
+}
+
+
+const removeItems =()=>{
+    
+}
+
     return (
         <>
             <div className='bg-[#6989aa] w-full  h-screen m-0 p-0 '>
@@ -18,13 +45,15 @@ function Todo() {
                                 <input
                                     className='text-center outline-none md:w-full '
                                     type="text"
+                                    value={list}
                                     placeholder='Add a items'
+                                    onChange={handleItems}
                                 />
 
                                 <button>
 
                                     <span>
-                                                        <IoIosAddCircle className='text-purple-500 text-3xl md:text-4xl  mr-4'/>
+                                                        <IoIosAddCircle className='text-purple-500 text-3xl md:text-4xl  mr-4' onClick={handleClick}/>
 
                                     </span>
 
@@ -34,12 +63,20 @@ function Todo() {
                             <div className=' lg:w-[60%] md:w-[55%] lg:h-[3px] md:h-[2px] bg-purple-500 mt-2 flex  ml-11 justify-center items-center'>
 
                             </div>
+{
+    items.map((mytodoItems,index)=>{
+        
+return(<>
 
-<div className='flex justify-start mt-4 ml-11 text-purple-500 text-base md:text-lg font-semibold  md:font-medium'>
+<div key={index} className='flex justify-start mt-4 ml-11 text-purple-500 text-base md:text-lg font-semibold  md:font-medium'>
     
-<IoMdCloseCircle className=' mr-2 text-2xl mt-[3px]' />    
-    Buy Apple</div>
-    
+<IoMdCloseCircle onClick={removeItems} className=' mr-2 text-2xl mt-[3px]' />    
+    {mytodoItems}</div>
+
+</>)
+    })
+
+ }   
 
                 </div>
 
